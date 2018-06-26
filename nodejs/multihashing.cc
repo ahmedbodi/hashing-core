47c928f618d5d0a1a081a846171272d02da2952c
Ahmed Bodiwala <ahmed_bodi@msn.com>
2018-06-26 00:03:54 +0000

    Add More Algorithm Support to library and update test script

--- nodejs/multihashing.cc
+++ nodejs/multihashing.cc
@@ -4,9 +4,7 @@
 #include <stdint.h>
 
 extern "C" {
-    #include "../algos/a5a.h"
     #include "../algos/blake.h"
-    #include "../algos/hive.h"
     #include "../algos/lyra2v2.h"
     #include "../algos/phi.h"
     #include "../algos/sib.h"
@@ -17,7 +15,6 @@ extern "C" {
     #include "../algos/hmq17.h"
     #include "../algos/lyra2z.h"
     #include "../algos/polytimos.h"
-    #include "../algos/skein2.h"
     #include "../algos/veltor.h"
     #include "../algos/x14.h"
     #include "../algos/aergo.h"
@@ -26,11 +23,9 @@ extern "C" {
     #include "../algos/Lyra2-z.h"
     #include "../algos/pomelo.h"
     #include "../algos/skein.h"
-    #include "../algos/velvet.h"
     #include "../algos/x15.h"
     #include "../algos/allium.h"
     #include "../algos/jha.h"
-    #include "../algos/m7m.h"
     #include "../algos/quark.h"
     #include "../algos/skunk.h"
     #include "../algos/vitalium.h"
@@ -45,7 +40,6 @@ extern "C" {
     #include "../algos/x16s.h"
     #include "../algos/bastion.h"
     #include "../algos/drop.h"
-    #include "../algos/lbry.h"
     #include "../algos/neoscrypt.h"
     #include "../algos/scryptn.h"
     #include "../algos/scrypt.h"
@@ -64,7 +58,6 @@ extern "C" {
     #include "../algos/pentablake.h"
     #include "../algos/sha256t.h"
     #include "../algos/x11.h"
-    #include "../algos/yescrypt.h"
     #include "../algos/blakecoin.h"
     #include "../algos/groestl.h"
     #include "../algos/lyra2re.h"
@@ -95,9 +88,7 @@ extern "C" {
     #include "../algos/sha3/sph_keccak.h"
     #include "../algos/sha3/sph_shabal.h"
     #include "../algos/sha3/sph_tiger.h"
-
 }
-
 
 using namespace node;
 using namespace v8;
@@ -184,19 +175,103 @@ using namespace v8;
  DECLARE_CALLBACK(xevan, xevan_hash, 32);
  DECLARE_CALLBACK(x16r, x16r_hash, 32);
  DECLARE_CALLBACK(x16s, x16s_hash, 32);
+ DECLARE_CALLBACK(timetravel, timetravel_hash, 32);
+ DECLARE_CALLBACK(bitcore, timetravel10_hash, 32);
+ DECLARE_CALLBACK(hsr, hsr_hash, 32);
+ DECLARE_CALLBACK(hmq1725, hmq17_hash, 32);
+ DECLARE_CALLBACK(jha, jha_hash, 32);
+ DECLARE_CALLBACK(allium, allium_hash, 32);
+ DECLARE_CALLBACK(lyra2, lyra2re_hash, 32);
+ DECLARE_CALLBACK(lyra2v2, lyra2v2_hash, 32);
+ DECLARE_CALLBACK(lyra2z, lyra2z_hash, 32);
+ DECLARE_CALLBACK(bastion, bastion_hash, 32);
+ DECLARE_CALLBACK(blake, blake_hash, 32);
+ DECLARE_CALLBACK(blake2s, blake2s_hash, 32);
+ DECLARE_CALLBACK(vanilla, blakecoin_hash, 32);
+ DECLARE_CALLBACK(decred, decred_hash, 32);
+ DECLARE_CALLBACK(deep, deep_hash, 32);
+ DECLARE_CALLBACK(fresh, fresh_hash, 32);
+ DECLARE_CALLBACK(quark, quark_hash, 32);
+ DECLARE_CALLBACK(nist5, nist5_hash, 32);
+ DECLARE_CALLBACK(qubit, qubit_hash, 32);
+ DECLARE_CALLBACK(groestl, groestl_hash, 32);
+ DECLARE_CALLBACK(skein, skein_hash, 32);
+ DECLARE_CALLBACK(sonoa, sonoa_hash, 32);
+ DECLARE_CALLBACK(tribus, tribus_hash, 32);
+ DECLARE_CALLBACK(keccak, keccak256_hash, 32);
+ DECLARE_CALLBACK(keccakc, keccak256_hash, 32);
+ DECLARE_CALLBACK(phi, phi_hash, 32);
+ DECLARE_CALLBACK(phi2, phi2_hash, 32);
+ DECLARE_CALLBACK(polytimos, polytimos_hash, 32);
+ DECLARE_CALLBACK(skunk, skunk_hash, 32);
+ DECLARE_CALLBACK(bmw, bmw_hash, 32);
+ DECLARE_CALLBACK(luffa, luffa_hash, 32);
+ DECLARE_CALLBACK(penta, penta_hash, 32);
+ DECLARE_CALLBACK(zr5, zr5_hash, 32);
+ DECLARE_CALLBACK(veltor, veltor_hash, 32);
+ DECLARE_CALLBACK(vitalium, vitalium_hash, 32);
+ DECLARE_CALLBACK(aergo, aergo_hash, 32);
+ DECLARE_CALLBACK(sib, sib_hash, 32);
+ DECLARE_CALLBACK(whirlpoolx, whirlpoolx_hash, 32);
+ DECLARE_CALLBACK(scrypt, scrypt_hash, 32);
+ DECLARE_CALLBACK(scryptn, scryptn_hash, 32);
+ DECLARE_CALLBACK(neoscrypt, neoscrypt_hash, 32);
 
 
 DECLARE_INIT(init) {
-    NODE_SET_METHOD(exports, "x11", x11);
-    NODE_SET_METHOD(exports, "x12", x12);
-    NODE_SET_METHOD(exports, "x13", x13);
-    NODE_SET_METHOD(exports, "x14", x14);
-    NODE_SET_METHOD(exports, "x15", x15);
-    NODE_SET_METHOD(exports, "x17", x17);
-    NODE_SET_METHOD(exports, "x11evo", x11evo);
-    NODE_SET_METHOD(exports, "xevan", xevan);
-    NODE_SET_METHOD(exports, "x16r", x16r);
-    NODE_SET_METHOD(exports, "x16s", x16s);
+ NODE_SET_METHOD(exports, "x11", x11);
+ NODE_SET_METHOD(exports, "c11", c11);
+ NODE_SET_METHOD(exports, "x11", x11);
+ NODE_SET_METHOD(exports, "x12", x12);
+ NODE_SET_METHOD(exports, "x13", x13);
+ NODE_SET_METHOD(exports, "x14", x14);
+ NODE_SET_METHOD(exports, "x15", x15);
+ NODE_SET_METHOD(exports, "x17", x17);
+ NODE_SET_METHOD(exports, "x11evo", x11evo);
+ NODE_SET_METHOD(exports, "xevan", xevan);
+ NODE_SET_METHOD(exports, "x16r", x16r);
+ NODE_SET_METHOD(exports, "x16s", x16s);
+ NODE_SET_METHOD(exports, "timetravel", timetravel);
+ NODE_SET_METHOD(exports, "bitcore", bitcore);
+ NODE_SET_METHOD(exports, "hsr", hsr);
+ NODE_SET_METHOD(exports, "hmq1725", hmq1725);
+ NODE_SET_METHOD(exports, "jha", jha);
+ NODE_SET_METHOD(exports, "allium", allium);
+ NODE_SET_METHOD(exports, "lyra2", lyra2);
+ NODE_SET_METHOD(exports, "lyra2v2", lyra2v2);
+ NODE_SET_METHOD(exports, "lyra2z", lyra2z);
+ NODE_SET_METHOD(exports, "bastion", bastion);
+ NODE_SET_METHOD(exports, "blake", blake);
+ NODE_SET_METHOD(exports, "blake2s", blake2s);
+ NODE_SET_METHOD(exports, "vanilla", vanilla);
+ NODE_SET_METHOD(exports, "decred", decred);
+ NODE_SET_METHOD(exports, "deep", deep);
+ NODE_SET_METHOD(exports, "fresh", fresh);
+ NODE_SET_METHOD(exports, "quark", quark);
+ NODE_SET_METHOD(exports, "nist5", nist5);
+ NODE_SET_METHOD(exports, "qubit", qubit);
+ NODE_SET_METHOD(exports, "groestl", groestl);
+ NODE_SET_METHOD(exports, "skein", skein);
+ NODE_SET_METHOD(exports, "sonoa", sonoa);
+ NODE_SET_METHOD(exports, "tribus", tribus);
+ NODE_SET_METHOD(exports, "keccak", keccak);
+ NODE_SET_METHOD(exports, "keccakc", keccakc);
+ NODE_SET_METHOD(exports, "phi", phi);
+ NODE_SET_METHOD(exports, "phi2", phi2);
+ NODE_SET_METHOD(exports, "polytimos", polytimos);
+ NODE_SET_METHOD(exports, "skunk", skunk);
+ NODE_SET_METHOD(exports, "bmw", bmw);
+ NODE_SET_METHOD(exports, "luffa", luffa);
+ NODE_SET_METHOD(exports, "penta", penta);
+ NODE_SET_METHOD(exports, "zr5", zr5);
+ NODE_SET_METHOD(exports, "veltor", veltor);
+ NODE_SET_METHOD(exports, "vitalium", vitalium);
+ NODE_SET_METHOD(exports, "aergo", aergo);
+ NODE_SET_METHOD(exports, "sib", sib);
+ NODE_SET_METHOD(exports, "whirlpoolx", whirlpoolx);
+ NODE_SET_METHOD(exports, "scrypt", scrypt);
+ NODE_SET_METHOD(exports, "scryptn", scryptn);
+ NODE_SET_METHOD(exports, "neoscrypt", neoscrypt);
 }
 
 NODE_MODULE(multihashing, init)
--- nodejs/test.js
+++ nodejs/test.js
@@ -2,6 +2,18 @@ var multiHashing = require('bindings')('multihashing.node')
 
 var data = new Buffer("010000000000000000000000000000000000000000000000000000000000000000000000e9d2350e20e3b9fb412a4f72e8602f331a77eede6dc200c2771d6cd1a4dd5f1834ece15affff7f1ee3ce0100", "hex");
 
-var hashedData = multiHashing.x11(data, 1, 4096, 1);
+var hashedData = multiHashing.x11evo(data, 1, 4096, 1);
+var algos = [
+    "c11", "x11", "x12", "x13", "x14", "x15", "x17", "x11evo", "xevan", "x16r", "x16s",
+    "timetravel", "bitcore", "hsr", "hmq1725", "jha", "allium", "lyra2", "lyra2v2", "lyra2z", "bastion",
+    "blake", "blake2s", "vanilla", "decred", "deep", "fresh", "quark", "qubit", "nist5", "groestl", "skein",
+    "sonoa", "tribus", "keccak", "keccakc", "phi", "phi2", "polytimos", "skunk", "bmw", "luffa", "penta",
+    "zr5", "veltor", "vitalium", "aergo", "sib", "whirlpoolx", "scrypt", "scryptn", "neoscrypt"
+];
 
-console.log(hashedData.toString('hex'));
+
+for(var i = 0; i < algos.length; i++) {
+   var algo = algos[i];
+   var hashedData = multiHashing[algo](data, 1, 4096, 1);
+   console.log("Algo: " + algo + " Hash: " + hashedData.toString('hex'));
+}
